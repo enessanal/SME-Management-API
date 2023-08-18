a6ffd169a1fd4a9e9d57b41464d63bcce5895931
ES <enessanal@protonmail.com>
2023-08-18 23:15:43 +0300

    CrossOrigin: Allowed all

---
+++ src/main/java/com/kz/sme_management/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.kz.sme_management.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer
+{
+    ///*TODO
+    //*  Restrict CrossOrigin in production!*/
+    @Override
+    public void addCorsMappings(CorsRegistry registry)
+    {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
