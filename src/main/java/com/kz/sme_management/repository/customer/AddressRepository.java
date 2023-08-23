8eaf6128de29e9e5abcd5bb82ff7dc06170e57eb
ES <enessanal@protonmail.com>
2023-08-23 23:17:59 +0300

    Added @Repository annotation to AddressRepository

--- src/main/java/com/kz/sme_management/repository/customer/AddressRepository.java
+++ src/main/java/com/kz/sme_management/repository/customer/AddressRepository.java
@@ -3,7 +3,9 @@ package com.kz.sme_management.repository.customer;
 import com.kz.sme_management.model.customer.Address;
 import com.kz.sme_management.model.customer.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address, String>
 {
     void deleteAddressesByCustomer(Customer customer);
