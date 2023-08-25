82f8f3de276e7d5585f71ff4a59e98cf4296bde9
ES <enessanal@protonmail.com>
2023-08-25 18:42:09 +0300

    Removed unused imports

--- src/main/java/com/kz/sme_management/controller/BrandController.java
+++ src/main/java/com/kz/sme_management/controller/BrandController.java
@@ -1,20 +1,16 @@
 package com.kz.sme_management.controller;
 
-import com.kz.sme_management.dto.product.ListProductDto;
 import com.kz.sme_management.model.product.Brand;
 import com.kz.sme_management.service.product.impl.BrandService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
