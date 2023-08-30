64066d59b4af15e9e60dfc9f669b45fc74a402ae
ES <enessanal@protonmail.com>
2023-08-30 21:26:21 +0300

    removed unused imports

--- src/main/java/com/kz/sme_management/service/product/IProductService.java
+++ src/main/java/com/kz/sme_management/service/product/IProductService.java
@@ -7,11 +7,10 @@ import org.springframework.data.domain.Page;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 public interface IProductService
 {
-    public Page<ListProductDto> findAll(Map<String, String> allParameters);
+    Page<ListProductDto> findAll(Map<String, String> allParameters);
     Product findById(String id);
 
     List<Product> findProductsByBrand(Brand brand);
