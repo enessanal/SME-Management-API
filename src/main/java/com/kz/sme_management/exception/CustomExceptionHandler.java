7a8ad369f97596fd39b07a8a6d6b254b397cd135
ES <enessanal@protonmail.com>
2023-09-12 23:31:49 +0300

    Added DataIntegrityViolationException

--- src/main/java/com/kz/sme_management/exception/CustomExceptionHandler.java
+++ src/main/java/com/kz/sme_management/exception/CustomExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.kz.sme_management.exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -7,7 +8,9 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -15,6 +18,28 @@ import java.util.stream.Collectors;
 @ControllerAdvice
 public class CustomExceptionHandler
 {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<?> handleDataIntegrityViolation(DataIntegrityViolationException ex, WebRequest request) {
+        Map<String, Object> errorDetails = new HashMap<>();
+        errorDetails.put("timestamp", LocalDateTime.now());
+        errorDetails.put("status", HttpStatus.BAD_REQUEST.value());
+        errorDetails.put("error", "Data Integrity Violation");
+        errorDetails.put("message", ex.getMostSpecificCause().getMessage());
+        errorDetails.put("path", request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+
+
+
+
+
+
+
+
+
+
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Object> handleException(MethodArgumentNotValidException ex)
